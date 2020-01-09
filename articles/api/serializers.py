c55397aed73e5a53301b8525403c3a02a2f8c3c5
raoufboussa <fa_boussa@esi.dz>
2020-01-09 16:58:16 +0100

    get data from the api

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -4,4 +4,4 @@ from ..models import Article
 class ArticleSerializers(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields  = '__all__'
+        fields  = ('id','title','content')
